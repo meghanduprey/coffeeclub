a17e1d527e2ae5ef9f68a68625cda499287a7f06
meghanduprey <meghan.duprey@gmail.com>
2019-09-09 11:03:29 -0400

    First commit for the coffee-club. File structure in place

---
+++ private/shared/header.php
@@ -0,0 +1,23 @@
+<?php
+  if(!isset($page_title)) { $page_title = 'Member Area'; }
+?>
+
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>Coffee Club - <?php echo h($page_title); ?></title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/staff.css'); ?>" />
+  </head>
+
+  <body>
+    <header>
+      <h1>Coffee Club Member Area</h1>
+    </header>
+
+    <nav>
+      <ul>
+        <li><a href="<?php echo url_for('/members/index.php'); ?>">Menu</a></li>
+      </ul>
+    </nav>
